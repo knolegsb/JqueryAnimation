edc612adbcd00ed2309b029e6f1b079e382a63e2
knolegsb <seon2you@hotmail.com>
2015-09-30 16:23:38 -0700

    added radial gradient

--- JqueryAnimation/CreatingAnimatedButton/ProjectSetup.js
+++ JqueryAnimation/CreatingAnimatedButton/ProjectSetup.js
@@ -1,6 +1,17 @@
 ﻿(function (ns) {
     ns.BuyNowButton = function (id) {
 
+        var b = $("#" + id);
+
+        setInterval(function () {
+            $({ xval: -30 }).animate({ xval: 300 },
+                {
+                    duration: 2000,
+                    step: function (now) {
+                        b.css('background', 'radial-gradient(circle 80px at ' + now + 'px 30px, #ffaaaa, red)')
+                    }
+                })
+        }, 2000);
 
     }
 }(window.PS = window.PS || {}));
