3e3b966032d85af6daad5a53e6f73aea7c6c4a0b
knolegsb <seon2you@hotmail.com>
2015-10-01 02:39:01 -0700

    added hover and normal

--- JqueryAnimation/AnimatedPictureGallery/Gallery.js
+++ JqueryAnimation/AnimatedPictureGallery/Gallery.js
@@ -22,6 +22,46 @@
                 xPos = 50;
                 yPos += 115;
             }
+
+            img.hover(
+                function (event) {
+                    var target = $(event.target);
+                    animateImage(target, 500, target.data('homeX'), target.data('homeY'), 2);
+                },
+                function (event) {
+                    returnAllToNormal();
+                }
+            );
         });
+
+        var returnAllToNormal = function () {
+            images.each(function (idx, el) {
+                var img = $(el);
+                animateImage(img, 500, img.data('homeX'), img.data('homeY'), 1);
+            });
+        }
+
+        var animateImage = function (img, duration, left, top, scale) {
+            img.stop();
+            img.css('textIndent', img.data('currentScale'));
+            img.animate({
+                'left': left,
+                'top': top,
+                'textIndent': scale,
+            },
+            {
+                duration: duration,
+                step: function (now, fx) {
+                    if (fx.prop === 'textIndent') {
+                        img.data('currentScale', now);
+                        img.css('transform', 'scale(' + now + ')');
+                    }
+                    else if (fx.prop === 'left')
+                        img.css('left', now);
+                    else if (fx.prop === 'top')
+                        img.css('top', now);
+                }
+            });
+        }
     }
 }(window.PS = window.PS || {}));
